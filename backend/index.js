20283b23148ef1f01492c80e2ba142461b4d639e
Nitin <nk17oct@gmail.com>
2022-12-20 02:33:39 +0530

    express and mongoose config

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,19 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB_URL || 'mongodb://localhost:27017/acadapp');
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", ()=>{
+    console.log("Database connected");
+});
 
 const app = express();
+
+app.use(express.urlencoded({extended:true}));
 
 app.get('/', (req,res)=>{
     res.send("Hello, world!");
